5137960c848c33c49ef00b60486bdf1dc233760e
woosub-lee <woosub.project@gmail.com>
2021-06-23 19:05:59 +0900

    #fb05 layout refactoring

--- Temp.py
+++ Temp.py
@@ -1,61 +1,79 @@
 import sys
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QLineEdit
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QLineEdit, QApplication, \
+    QFileDialog, QMainWindow, QAction
 from win32api import GetSystemMetrics
 
 
 class ComponentFactory:
-    def makeLable(self, content):
+    @staticmethod
+    def makeLable(content):
         return QLabel(content)
 
-    def makeButton(self, content):
+    @staticmethod
+    def makeButton(content):
         return QPushButton(content)
 
-    def makeEditor(self):
+    @staticmethod
+    def makeEditor():
         return QLineEdit()
 
 
 class LayoutFactory:
-    def makeVBox(self):
+    @staticmethod
+    def makeVBox():
         return QVBoxLayout()
 
-    def makeHBox(self):
+    @staticmethod
+    def makeHBox():
         return QHBoxLayout()
 
 class MyLayout(QWidget):
     def __init__(self):
         super().__init__()
-
+        self.makeComponent()
+        self.makeBoxLayout()
+        self.makeComponentsList()
+        self.fillVBoxLayout()
+        self.setLayout(self.boxLayout)
 
     def makeComponent(self):
-        componentFactory = ComponentFactory()
+        self.questionLabel = ComponentFactory.makeLable("지금 할 코딩은?")
+        self.titleLabel = ComponentFactory.makeLable("")
+        self.titleEditor = ComponentFactory.makeEditor()
+        self.setTitleButton = ComponentFactory.makeButton("결정")
 
-        self.questionLabel = componentFactory.makeLable("지금 할 코딩은?")
-        self.titleLabel = componentFactory.makeLable("")
-        self.titleEditor = componentFactory.makeEditor()
-        self.setTitleButton = componentFactory.makeButton("결정")
+        self.warningLable = ComponentFactory.makeLable("")
 
-        self.warningLable = componentFactory.makeLable("")
+        self.timeLabel = ComponentFactory.makeLable("00:00:00")
 
-        self.timeLabel = componentFactory.makeLable("00:00:00")
-
-        self.startTimerButton = componentFactory.makeButton("시작")
-        self.stopTimerButton = componentFactory.makeButton("정지")
-        self.recordTimerButton = componentFactory.makeButton("기록")
+        self.startTimerButton = ComponentFactory.makeButton("시작")
+        self.stopTimerButton = ComponentFactory.makeButton("정지")
+        self.recordTimerButton = ComponentFactory.makeButton("기록")
 
     def makeComponentsList(self):
-        self.ComponentsH1 = []
+        self.componentsH1 = [self.questionLabel,
+                             self.titleLabel,
+                             self.titleEditor,
+                             self.setTitleButton]
+
+        self.componentsH2 = [self.warningLable]
+
+        self.componentsH3 = [self.timeLabel]
+
+        self.componentsH4 = [self.startTimerButton,
+                             self.stopTimerButton,
+                             self.recordTimerButton]
 
     def makeBoxLayout(self):
-        layoutFactory = LayoutFactory()
+        self.boxLayoutH1 = LayoutFactory.makeHBox()
+        self.boxLayoutH2 = LayoutFactory.makeHBox()
+        self.boxLayoutH3 = LayoutFactory.makeHBox()
+        self.boxLayoutH4 = LayoutFactory.makeHBox()
 
-        self.boxLayoutH1 = layoutFactory.makeHBox()
-        self.boxLayoutH2 = layoutFactory.makeHBox()
-        self.boxLayoutH3 = layoutFactory.makeHBox()
-        self.boxLayoutH4 = layoutFactory.makeHBox()
-
-        self.boxLayout = layoutFactory.makeVBox()
+        self.boxLayout = LayoutFactory.makeVBox()
 
 
     def fillVBoxLayout(self):
@@ -64,21 +82,14 @@ class MyLayout(QWidget):
             for component in components:
                 layoutBox.addWidget(component)
             layoutBox.addStretch(1)
-        fillHBoxLayout(self.boxLayoutH1)
-
-        self.boxLayoutH2.addStretch(1)
-        self.boxLayoutH2.addWidget(self.warningLable)
-        self.boxLayoutH2.addStretch(1)
-
-        self.boxLayoutH3.addStretch(1)
-        self.boxLayoutH3.addWidget(self.timeLabel)
-        self.boxLayoutH3.addStretch(1)
-
-        self.boxLayoutH4.addStretch(1)
-        self.boxLayoutH4.addWidget(self.startTimerButton)
-        self.boxLayoutH4.addWidget(self.stopTimerButton)
-        self.boxLayoutH4.addWidget(self.recordTimerButton)
-        self.boxLayoutH4.addStretch(1)
+        fillHBoxLayout(self.boxLayoutH1, self.componentsH1)
+        fillHBoxLayout(self.boxLayoutH2, self.componentsH2)
+        fillHBoxLayout(self.boxLayoutH3, self.componentsH3)
+        fillHBoxLayout(self.boxLayoutH4, self.componentsH4)
+        self.boxLayout.addLayout(self.boxLayoutH1)
+        self.boxLayout.addLayout(self.boxLayoutH2)
+        self.boxLayout.addLayout(self.boxLayoutH3)
+        self.boxLayout.addLayout(self.boxLayoutH4)
 
 
 
